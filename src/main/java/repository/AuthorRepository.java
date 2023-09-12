f8426b2a137e80e08a1e5c6fbb6dc895a6fdd33a
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-12 14:51:19 +0330

    Write save Method

--- src/main/java/repository/AuthorRepository.java
+++ src/main/java/repository/AuthorRepository.java
@@ -1,8 +1,10 @@
 package repository;
 
+import author.Author;
 import jdbcConnection.JdbcConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AuthorRepository {
@@ -11,6 +13,16 @@ public class AuthorRepository {
     Connection connection=jdbcConnection.getConnection();
 
     public AuthorRepository() throws SQLException {
+    }
+
+    public int save(Author author) throws SQLException {
+        String sql="INSERT INTO author(firstname, lastnname, age) VALUES (?,?,?)";
+        PreparedStatement preparedStatement= connection.prepareStatement(sql);
+        preparedStatement.setString(1, author.getFirstName());
+        preparedStatement.setString(2,author.getLastnName());
+        preparedStatement.setInt(3,author.getAge());
+        int result = preparedStatement.executeUpdate();
+        return result;
     }
 
 
