3155728672ba4cdacfc3149739b1693aeae3746c
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-12 16:35:16 +0330

    write Book load method

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -6,6 +6,7 @@ import jdbcConnection.JdbcConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BookRepository {
@@ -24,5 +25,23 @@ public class BookRepository {
         preparedStatement.setString(3,book.getPrintYear());
         int result = preparedStatement.executeUpdate();
         return result;
+    }
+
+    public Book load(int bookId)throws SQLException{
+        String sql="SELECT * FROM book WHERE bookid=?";
+        PreparedStatement preparedStatement=connection.prepareStatement(sql);
+        ResultSet resultSet= preparedStatement.executeQuery();
+        if (resultSet.next()){
+            Book book = new Book(
+                    resultSet.getInt("bookId"),
+                    resultSet.getString("authorId"),
+                    resultSet.getString("title"),
+                    resultSet.getInt("printYear")
+            );
+            return book;
+        }else{
+            return null;
+        }
+
     }
 }
