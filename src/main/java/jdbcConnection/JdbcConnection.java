294fdefdb8c6492a245a949954826d504c703284
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-12 14:26:23 +0330

    Create JdbcConnetion Class and write Connection

---
+++ src/main/java/jdbcConnection/JdbcConnection.java
@@ -0,0 +1,18 @@
+package jdbcConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+
+    Connection connection= DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","173725Hrm");
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public JdbcConnection() throws SQLException {
+    }
+}
+
