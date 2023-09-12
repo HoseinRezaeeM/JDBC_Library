a1e7f7f588000052c393b1dbad9466b5951a5945
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-12 15:20:19 +0330

    Create BookRepository and write save method

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package repository;
 
+import author.Author;
+import book.Book;
 import jdbcConnection.JdbcConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class BookRepository {
@@ -11,5 +14,15 @@ public class BookRepository {
     Connection connection=jdbcConnection.getConnection();
 
     public BookRepository() throws SQLException {
+    }
+
+    public int save(Book book) throws SQLException {
+        String sql="INSERT INTO book(authorid, title, printyear) VALUES (?,?,?)";
+        PreparedStatement preparedStatement= connection.prepareStatement(sql);
+        preparedStatement.setInt(1, book.getAuthorId());
+        preparedStatement.setString(2,book.getTitle());
+        preparedStatement.setString(3,book.getPrintYear());
+        int result = preparedStatement.executeUpdate();
+        return result;
     }
 }
