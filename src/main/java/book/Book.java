1e490cf7c20fe8b45b2924c7989d93619a9378db
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-12 14:57:51 +0330

    Write bookId properties

--- src/main/java/book/Book.java
+++ src/main/java/book/Book.java
@@ -1,14 +1,25 @@
 package book;
 
 public class Book {
+
+    private Integer bookId;
     private Integer authorId;
     private String title;
     private String printYear;
 
-    public Book(Integer authorId, String title, String printYear) {
+    public Book(Integer authorId, String title, String printYear,Integer bookId) {
         this.authorId = authorId;
         this.title = title;
         this.printYear = printYear;
+        this.bookId=bookId;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
     }
 
     public Integer getAuthorId() {
