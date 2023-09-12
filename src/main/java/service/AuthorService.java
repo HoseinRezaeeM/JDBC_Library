93d10724c658f2d7a69304bdaa7decbed5e35b43
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-12 15:11:05 +0330

    Create AuthorService and write register method

---
+++ src/main/java/service/AuthorService.java
@@ -0,0 +1,26 @@
+package service;
+
+import author.Author;
+import repository.AuthorRepository;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AuthorService {
+
+    final AuthorRepository authorRepository=new AuthorRepository();
+    Scanner scanner=new Scanner(System.in);
+
+    public AuthorService() throws SQLException {
+    }
+
+    public void register(Author author) throws SQLException {
+        int result=authorRepository.save(author);
+        if(result != 0)
+            System.out.println(author.getFirstName() +" "+author.getLastnName()+ " successfully added to database");
+        else
+            System.out.println("Don't add to database!! ");
+
+    }
+
+}
