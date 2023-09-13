5dc1442495e8dc19f539961592e98d3b247a74de
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-13 16:12:03 +0330

    Edit AuthorService

--- src/main/java/service/AuthorService.java
+++ src/main/java/service/AuthorService.java
@@ -4,29 +4,25 @@ import author.Author;
 import repository.AuthorRepository;
 
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class AuthorService {
 
-    final AuthorRepository authorRepository=new AuthorRepository();
-    Scanner scanner=new Scanner(System.in);
+    final AuthorRepository authorRepository = new AuthorRepository();
 
     public AuthorService() throws SQLException {
     }
 
     public void register(Author author) throws SQLException {
-        int result=authorRepository.save(author);
-        if(result != 0)
-            System.out.println(author.getFirstName() +" "+author.getLastnName()+ " successfully added to database");
-        else
-            System.out.println("Don't add to database!! ");
+        int result = authorRepository.save(author);
+        if (result != 0)
+            System.out.println(author.getFirstName() + " " + author.getLastnName() + " successfully added to database");
+        else System.out.println("Don't add to database!! ");
 
     }
 
-    public void load(int authorId)throws SQLException{
+    public void load(int authorId) throws SQLException {
 
         authorRepository.load(authorId);
-
     }
 
 }
